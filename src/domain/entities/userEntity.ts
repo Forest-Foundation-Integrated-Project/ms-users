1c7d75cb9ae927ebdeeb15d5bcba61047d00fd96
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2023-04-02 02:12:01 -0300

    feat: add user entity

---
+++ src/domain/entities/userEntity.ts
@@ -0,0 +1,29 @@
+// import { v4 as uuid } from 'uuid'
+import { randomUUID } from 'crypto'
+
+import { Either, right } from '../../framework/shared/either'
+import { IError } from '../../framework/shared/iError'
+import { AbstractEntity } from './abstractEntity'
+
+export interface IUserEntity {
+  userId?: string
+  name: string
+  username: string
+  birthDate: Date
+  password?: string
+  enroll: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class UserEntity extends AbstractEntity<IUserEntity> {
+  static create(props: IUserEntity): Either<IError, UserEntity> {
+    const user = new UserEntity({
+      ...props,
+      userId: randomUUID(),
+      createdAt: new Date(),
+    })
+
+    return right(user)
+  }
+}
