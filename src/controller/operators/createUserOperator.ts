1f5d32bc03a9fdaa061f0d8c9ae54997818990a4
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2023-04-02 02:17:43 -0300

    feat: add create user operator

---
+++ src/controller/operators/createUserOperator.ts
@@ -0,0 +1,23 @@
+import { injectable, inject } from 'inversify'
+
+import { InputCreateUser, OutputCreateUser } from '../serializers/inputCreateUser'
+import { left, right } from '../../framework/shared/either'
+import { AbstractOperator } from './abstractOperator'
+import { CreateUserUseCase } from '../../business/useCases/createUserUseCase'
+
+@injectable()
+export class CreateUserOperator extends AbstractOperator<InputCreateUser, OutputCreateUser> {
+  public constructor(@inject(CreateUserUseCase) private createUserUseCase: CreateUserUseCase) {
+    super()
+  }
+
+  protected async run(input: InputCreateUser): Promise<OutputCreateUser> {
+    const result = await this.createUserUseCase.exec(input)
+
+    if (result.isLeft()) {
+      return left(result.value)
+    }
+
+    return right(result.value)
+  }
+}
