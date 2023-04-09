b29f6668806d5ac18c79f69cd69d760a30ceeb13
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-04-08 23:53:02 -0300

    chore: configure sequelize

---
+++ devops/migrations/20230409020841-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      user_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      enroll: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birth_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
