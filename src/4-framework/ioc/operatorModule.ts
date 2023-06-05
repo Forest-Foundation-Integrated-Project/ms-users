2db1a6f772046310a0ebae0722a87391e5581361
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-05 14:09:27 -0300

    chore: configure reset password lambda binds

--- src/4-framework/ioc/operatorModule.ts
+++ src/4-framework/ioc/operatorModule.ts
@@ -4,10 +4,12 @@ import { CreateUserOperator } from '../../3-controller/operators/createUserOpera
 import { RemoveUserOperator } from '../../3-controller/operators/removeUserOperator'
 import { UpdateUserOperator } from '../../3-controller/operators/updateUserOperatos'
 import { ViewUserOperator } from '../../3-controller/operators/viewUserOperator'
+import { ResetPasswordOperator } from '../../3-controller/operators/resetPasswordOperator'
 
 export const OperatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateUserOperator).toSelf()
   bind(ViewUserOperator).toSelf()
   bind(RemoveUserOperator).toSelf()
   bind(UpdateUserOperator).toSelf()
+  bind(ResetPasswordOperator).toSelf()
 })
--- src/4-framework/ioc/useCasesModule.ts
+++ src/4-framework/ioc/useCasesModule.ts
@@ -3,10 +3,12 @@ import { CreateUserUseCase } from '../../2-business/useCases/createUserUseCase'
 import { RemoveUserUseCase } from '../../2-business/useCases/removeUserUseCase'
 import { UpdateUserUseCase } from '../../2-business/useCases/updateUserUseCase'
 import { ViewUserUseCase } from '../../2-business/useCases/viewUserUseCase'
+import { ResetPasswordUseCase } from '../../2-business/useCases/resetPasswordUseCase'
 
 export const UseCasesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateUserUseCase).toSelf()
   bind(ViewUserUseCase).toSelf()
   bind(RemoveUserUseCase).toSelf()
   bind(UpdateUserUseCase).toSelf()
+  bind(ResetPasswordUseCase).toSelf()
 })
