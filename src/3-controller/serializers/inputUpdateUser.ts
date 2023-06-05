095d3ffb785cbec30319468235c4afb518db30d6
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-05 13:38:11 -0300

    refactor: remove unused imports

--- src/3-controller/serializers/inputUpdateUser.ts
+++ src/3-controller/serializers/inputUpdateUser.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsDate, Matches, IsBoolean, IsOptional, IsUUID, IsIn } from 'class-validator'
+import { IsNotEmpty, IsString, IsDate, IsBoolean, IsOptional, IsUUID, IsIn } from 'class-validator'
 
 import { GenderTypes, IUserEntity } from '../../1-domain/entities/userEntity'
 import { Either } from '../../4-framework/shared/either'
