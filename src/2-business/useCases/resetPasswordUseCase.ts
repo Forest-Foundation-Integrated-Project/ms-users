0ecb355ae4fb250f86184236c52d43ea0fa30a73
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-06 12:25:22 -0300

    feat: add reset password email on function

--- src/2-business/useCases/resetPasswordUseCase.ts
+++ src/2-business/useCases/resetPasswordUseCase.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'inversify'
-//import { Lambda } from 'aws-sdk'
+import { Lambda } from 'aws-sdk'
 
 import { IUserRepository, IUserRepositoryToken } from '../repositories/iUserRepository'
 import { left, right } from '../../4-framework/shared/either'
@@ -7,11 +7,11 @@ import { HandlePassword } from './handler/handlerPassword'
 import { IUseCase } from './iUseCase'
 import { UserIdentityCannotBeValidated, UserNotFound, UserUpdateFailed } from '../module/errors/users'
 import { InputResetPasswordDto, OutputResetPasswordDto } from '../dto/userDto'
-//import { createToken } from './handler/createToken'
+import { createToken } from './handler/createToken'
 
 @injectable()
 export class ResetPasswordUseCase implements IUseCase<InputResetPasswordDto, OutputResetPasswordDto> {
-  //sendEmail = new Lambda
+  sendEmail = new Lambda
 
   public constructor(@inject(IUserRepositoryToken) private userRepository: IUserRepository) {}
 
@@ -30,18 +30,18 @@ export class ResetPasswordUseCase implements IUseCase<InputResetPasswordDto, Out
 
       if (!user) return left (UserNotFound)
 
-      /*const emailResponse = await this.sendEmail.invoke({
+      const emailResponse = await this.sendEmail.invoke({
         FunctionName: 'ms-emails-dev-sendMail',
         InvocationType: 'RequestResponse',
         LogType: 'None',
         Payload: JSON.stringify({
           targetEmail: input.email,
-          emailType: "confirmEmail",
+          emailType: "resetPassword",
           token: createToken()
         })
       }).promise()
 
-      console.log('email::response => ', emailResponse)*/
+      console.log('email::response => ', emailResponse)
 
       return right(!!user)
     } catch (error) {
