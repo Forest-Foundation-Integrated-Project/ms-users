a2f311b50ef29c2400a5f18ac471743cd9b76330
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-18 20:09:54 -0300

    feat: add confirm token lambda

---
+++ src/4-framework/functions/confirmToken.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata'
+import '../ioc/inversify.config'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+
+import { httpHandler } from '../utility/httpHandler'
+import { container } from '../shared/ioc/container'
+import { httpResponse } from '../utility/httpResponse'
+import { InputConfirmToken } from '../../3-controller/serializers/inputConfirmToken'
+import { ConfirmTokenOperator } from '../../3-controller/operators/confirmTokenOperator'
+
+export const handler = httpHandler(async (event: APIGatewayProxyEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  const operator = container.get(ConfirmTokenOperator)
+  const body = JSON.parse(event?.body as string)
+
+  const input = new InputConfirmToken(body)
+  const result = await operator.exec(input)
+
+  if (result.isLeft()) {
+    return httpResponse.badRequest(result.value)
+  }
+
+  return httpResponse.created(result.value)
+})
