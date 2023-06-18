c1e102e70eed90c35be99d1548261f03a1238014
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-18 19:45:24 -0300

    chore: add token validation and not found errors

--- src/2-business/module/errors/tokens/index.ts
+++ src/2-business/module/errors/tokens/index.ts
@@ -3,3 +3,15 @@ export const TokenConfirmationFailed = {
   message: 'Token confirmation failed',
   shortMessage: 'tokenConfirmationFailed'
 }
+
+export const TokenValidationFailed = {
+  code: 'TKV-001',
+  message: 'Token validation failed',
+  shortMessage: 'tokenValidationFailed'
+}
+
+export const TokenNotFound = {
+  code: 'TKC-002',
+  message: 'Token not found',
+  shortMessage: 'tokenNotFound'
+}
