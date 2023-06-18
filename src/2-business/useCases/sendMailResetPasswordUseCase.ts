1414b1af38899918b52ff89283c2a7a48daa65a5
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-18 20:11:07 -0300

    chore: add email validation to reset password

--- src/2-business/useCases/sendMailResetPasswordUseCase.ts
+++ src/2-business/useCases/sendMailResetPasswordUseCase.ts
@@ -8,40 +8,51 @@ import { InputSendMailResetPasswordDto, OutputSendMailResetPasswordDto } from '.
 import { createToken } from './handler/createToken'
 import { ITokenRepository, ITokenRepositoryToken } from '../repositories/iTokenRepository'
 import { OperationTypes } from '../../1-domain/entities/tokenEntity'
+import { IUserRepository, IUserRepositoryToken } from '../repositories/iUserRepository'
 
 @injectable()
 export class SendMailResetPasswordUseCase implements IUseCase<InputSendMailResetPasswordDto, OutputSendMailResetPasswordDto> {
   sendEmail = new Lambda
 
-  public constructor(@inject(ITokenRepositoryToken) private tokenRepository: ITokenRepository) {}
+  public constructor(
+    @inject(IUserRepositoryToken) private userRepository: IUserRepository,
+    @inject(ITokenRepositoryToken) private tokenRepository: ITokenRepository
+  ) { }
 
   async exec(input: InputSendMailResetPasswordDto): Promise<OutputSendMailResetPasswordDto> {
     try {
-      const tokenResult = await this.tokenRepository.create({
-        token: createToken(),
-        operationType: OperationTypes.sendMailResetPassword,
-        email: input.email,
-        expirationDate: Date.now() + 300000
-      })
 
-      console.log('token::result => ', tokenResult)
+      const existsResponse = await this.userRepository.getByEmail(input.email)
 
-      const emailResponse = await this.sendEmail.invoke({
-        FunctionName: 'ms-emails-dev-sendMail',
-        InvocationType: 'RequestResponse',
-        LogType: 'None',
-        Payload: JSON.stringify({
-          targetEmail: input.email,
-          emailType: "resetPassword",
-          token: tokenResult.token
+      if (existsResponse) {
+        const tokenResult = await this.tokenRepository.create({
+          token: createToken(),
+          operationType: OperationTypes.sendMailResetPassword,
+          email: input.email,
+          expirationDate: Date.now() + 300000
         })
-      }).promise()
 
-      console.log('email::response => ', emailResponse)
-      const payloadResponseString = JSON.parse(emailResponse.Payload as string)
-      const payloadResponse = JSON.parse(payloadResponseString.body)
+        console.log('token::result => ', tokenResult)
 
-      return right(payloadResponse)
+        const emailResponse = await this.sendEmail.invoke({
+          FunctionName: 'ms-emails-dev-sendMail',
+          InvocationType: 'RequestResponse',
+          LogType: 'None',
+          Payload: JSON.stringify({
+            targetEmail: input.email,
+            emailType: "resetPassword",
+            token: tokenResult.token
+          })
+        }).promise()
+
+        console.log('email::response => ', emailResponse)
+        const payloadResponseString = JSON.parse(emailResponse.Payload as string)
+        const payloadResponse = JSON.parse(payloadResponseString.body)
+
+        return right(payloadResponse)
+      }
+
+      return right(true)
     } catch (error) {
       return left(UserUpdateFailed)
     }
