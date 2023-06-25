ec7c39dd56b3092ebb6f57c911272ec6f9d34b2a
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-06-24 22:01:28 -0300

    chore: configure storage service binds

--- src/4-framework/ioc/servicesModule.ts
+++ src/4-framework/ioc/servicesModule.ts
@@ -3,7 +3,10 @@ import { ContainerModule, interfaces } from 'inversify'
 import { IIdentityService } from '../../2-business/services/iIdentityService'
 import { IIdentityServiceToken } from '../../2-business/services/iIdentityService'
 import { IdentityService } from '../services/identityService'
+import { IStorageService, IStorageServiceToken } from '../../2-business/services/iStorageService'
+import { StorageService } from '../services/storageService'
 
 export const ServicesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<IIdentityService>(IIdentityServiceToken).to(IdentityService)
+  bind<IStorageService>(IStorageServiceToken).to(StorageService)
 })
